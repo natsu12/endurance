8a9cff7969104fae8aa61e1d1270db6efb9bd298
natsu12 <natsu12@126.com>
2017-06-09 13:08:07 +0800

    test

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-$(function  () {
+$(function () {
   // $.get('http://localhost:8888/api/v1/test', function (data) {
   // $.get('https://sabox-server.nie.netease.com/api/v1/endurance', function (data) {
   //   if (data != '') {
   //     $('.question').html(data)
   //     console.log(data);
-      
+
   //   } else {
   //     $('.question').html('电视机上啥都没有哦')
   //   }
@@ -12,11 +12,20 @@ $(function  () {
 
   $.ajax({
 
-    url: 'https://sabox-server.nie.netease.com/api/v1/endurance',
+    url: 'http://localhost:8888/api/v1/endurance',
     type: 'GET',
     crossDomain: true,
     dataType: 'jsonp',
-    success: function (data) { alert("Success"); },
+    success: function (data) {
+      if (data != 'empty') {
+        $('.info').html(data)
+        $('.info').css('display', 'block');
+
+      } else {
+        $('.question').html('电视机上啥都没有哦');
+        $('.result').css('display', 'block');
+      }
+    },
     // error: function () { alert('Failed!'); },
   });
 })
