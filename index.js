e2a717524c5c3c82da408777967ca6449ea869e3
natsu12 <natsu12@126.com>
2017-06-09 10:58:39 +0800

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(function  () {
   // $.get('http://localhost:8888/api/v1/test', function (data) {
-  $.get('http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=13532012981', function (data) {
+  $.get('https://sabox-server.nie.netease.com/api/v1/endurance', function (data) {
     if (data != '') {
       $('.question').html(data)
       console.log(data);
