83f3b97bb127a6746700ccfc456fc6953ee5dbf6
natsu12 <natsu12@126.com>
2017-06-09 11:08:54 +0800

    test

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 $(function  () {
   // $.get('http://localhost:8888/api/v1/test', function (data) {
-  $.get('https://sabox-server.nie.netease.com/api/v1/endurance', function (data) {
-    if (data != '') {
-      $('.question').html(data)
-      console.log(data);
+  // $.get('https://sabox-server.nie.netease.com/api/v1/endurance', function (data) {
+  //   if (data != '') {
+  //     $('.question').html(data)
+  //     console.log(data);
       
-    } else {
-      $('.question').html('电视机上啥都没有哦')
-    }
-  })
+  //   } else {
+  //     $('.question').html('电视机上啥都没有哦')
+  //   }
+  // })
+
+  $.ajax({
+
+    url: 'https://sabox-server.nie.netease.com/api/v1/endurance',
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+    success: function (data) { alert("Success"); },
+    // error: function () { alert('Failed!'); },
+  });
 })
